0947c360d9623092cef7111430e7febed67bbb19
Z <shonsanthosh00@gmail.com>
2026-08-02 21:22:50 +0530

    Add number guessing game in game.py
    Implements a number guessing game with user input and attempts.

---
+++ projects/game.py
@@ -0,0 +1,52 @@
+import random
+
+print("guess the number game, you have 10 guesses")
+
+
+def function():
+
+    guess_no = random.randint(1, 100)
+    attempts = 10
+
+    while attempts > 0:
+        user_guess = input("Guess a number between 1 and 100: ")
+
+        try:
+            user_guess = int(user_guess)
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+            continue
+
+        if user_guess < 1 or user_guess > 100:
+            print("Please guess a number between 1 and 100.")
+            continue
+
+        attempts -= 1
+
+        if user_guess < guess_no:
+            print("Too low! Try again.")
+        elif user_guess > guess_no:
+            print("Too high! Try again.")
+        else:
+            print(
+                f"Congratulations! You guessed the number {guess_no} in {10 - attempts} attempts."
+            )
+            break
+        print(f"You have {attempts} attempts left.")
+    else:
+        print(f"Sorry, you've used all your attempts. The number was {guess_no}.")
+
+
+while True:
+    function()
+
+    try_again = input("want to try again?: Y/N").strip().upper()
+
+    if try_again in ("YES", "Y"):
+        continue
+    elif try_again in ("NO", "N"):
+        print("thank you!")
+        break
+    else:
+        print("thank you!")
+        break
